8b66997d768ebfed242bbad6014e7b237073aa95
LeonteDenis <denis.leonte123@gmail.com>
2026-02-19 18:49:12 +0200

    other changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -66,7 +66,7 @@ const Footer: React.FC = () => {
         </div>
 
         <div className="footer__credit">
-          <p>Îți place acest site? Contactează-ne la <a href="mailto:denistechs@gmail.com">denistechs@gmail.com</a></p>
+          <p>Îți place acest site? Contactează-mă la <a href="mailto:denistechs@gmail.com">denistechs@gmail.com</a></p>
         </div>
       </div>
     </footer>
